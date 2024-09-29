98883ca7c7b1b5b1cc05db04df1797959213707e
Richit Bhat <132209234+NotRichit@users.noreply.github.com>
2024-09-29 14:45:52 +0530

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -1,5 +1,8 @@
 RESET='\033[0m'       # Text Reset
-
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 # Regular Colors
 Black='\033[0;30m'        # Black
 Red='\033[0;31m'          # Red
